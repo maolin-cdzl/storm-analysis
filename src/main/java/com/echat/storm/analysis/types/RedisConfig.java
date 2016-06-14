ca1424f6ae20f2961dc4fe811d02b08b2e3c25cf
毛林 <maolin@shanlitech.com>
2016-06-14 13:51:26 +0800

    change redis host to 192.168.1.181

--- src/main/java/com/echat/storm/analysis/types/RedisConfig.java
+++ src/main/java/com/echat/storm/analysis/types/RedisConfig.java
@@ -3,7 +3,7 @@ package com.echat.storm.analysis.types;
 import java.io.Serializable;
 
 public class RedisConfig implements Serializable {
-	static public final String HOST = "localhost";
+	static public final String HOST = "192.168.1.181";
 	static public final int PORT = 6379;
 	static public final int TIMEOUT = 3000;
 }
